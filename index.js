efc74ffa5534df56d164fac1f17834c01ec999a0
NOURA_ALGAHTANI <Nourahaniq@gmail.com>
2023-04-28 14:11:38 +0300

    finished

--- index.js
+++ index.js
@@ -22,3 +22,21 @@ flipBtn.addEventListener("click", () => {
       setTimeout(updateStats, 3000);
       disableButton();
 });
+function updateStats() {
+    document.querySelector("#heads-count").textContent = `Heads: ${heads}`;
+    document.querySelector("#tails-count").textContent = `Tails: ${tails}`;
+}
+
+function disableButton() {
+    flipBtn.disabled = true;
+    setTimeout(function () {
+        flipBtn.disabled = false;
+    }, 3000);
+}
+
+resetBtn.addEventListener("click", () => {
+    coin.style.animation = "none";
+    heads = 0;
+    tails = 0;
+    updateStats();
+})
